258d9d661103e1f90ea2211fbb438e4eba3e1345
z5014823 <z5014823@users.noreply.github.com>
2019-03-28 01:12:44 +1100

    Add files via upload

---
+++ nn.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from load_CIFAR10 import load_CIFAR10
+from nn_classifier import TwoLayerNet
+
+def rel_error(x, y):
+    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+
+def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000):
+    cifar10_dir = 'cifar-10-batches-py'
+    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+    # Subsample the data
+    mask = range(num_training, num_training + num_validation)
+    X_val = X_train[mask]
+    y_val = y_train[mask]
+    mask = range(num_training)
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+    mask = range(num_test)
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+
+    # Normalize the data, subtract the mean image
+    mean_image = np.mean(X_train, axis=0)
+    X_train -= mean_image
+    X_val -= mean_image
+    X_test -= mean_image
+
+    # Reshape data to rows
+    X_train = X_train.reshape(num_training, -1)
+    X_val = X_val.reshape(num_validation, -1)
+    X_test = X_test.reshape(num_test, -1)
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+# Invoke the above function to get our data
+X_train, y_train, X_val, y_val, X_test, y_test = get_CIFAR10_data()
+print('Train data shape: ', X_train.shape)
+print('Train labels shape: ', y_train.shape)
+print('Validation data shape: ', X_val.shape)
+print('Validation labels shape: ', y_val.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+best_net = None
+input_size = 32*32*3  #1024*3
+hidden_size = [75, 100, 125] 
+num_classes = 10
+results = {}
+best_val_acc = 0
+best_net = None
+
+learning_rates = 1e-3 * np.array([0.7, 0.8, 0.9, 1, 1.1])
+regularisation_strengths = [0.75, 1, 1.25]
+
+print('running')
+for hs in hidden_size:
+    for lr in learning_rates:
+        for reg in regularisation_strengths:
+            print('.')
+            net = TwoLayerNet(input_size, hs, num_classes)
+            # Train the network
+            stats = net.train(X_train, y_train, X_val, y_val,
+                num_iters=1500, batch_size=200,
+                learning_rate=lr, learning_rate_decay=0.95,
+                reg=reg, verbose=False)
+            val_acc = (net.predict(X_val) == y_val).mean()
+            if val_acc > best_val_acc:
+                best_val_acc = val_acc
+                best_net = net
+            results[(hs, lr, reg)] = val_acc
+print('finished')
+for hs, lr, reg in sorted(results):
+    val_acc = results[(hs, lr, reg)]
+    print('hs %d lr %e reg %e val accuracy: %f' % (hs, lr, reg, val_acc))
+print('best validation accuracy achieved during cross-validation: %f' % best_val_acc)
+
+test_acc = (best_net.predict(X_test) == y_test).mean()
+print('Test accuracy: ', test_acc)
--- nn_classifier.py
+++ nn_classifier.py
@@ -18,7 +18,7 @@ class TwoLayerNet(object):
         self.params = {}
         self.params['W1'] = std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = std * np.random.randn(output_size, output_size)
+        self.params['W2'] = std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
     def loss(self, X, y=None, reg=0.0):
@@ -26,18 +26,110 @@ class TwoLayerNet(object):
         W2, b2 = self.params['W2'], self.params['b2']
         N, D = X.shape
 
+        # Compute the forward pass
         scores = None
-        out_1 = X.dot(W1) + b1
-        gate_1 = np.maximum(0, out_1)
-        out_2 = gate_1.dot(W2) + b2
-        scores = out_2 // (N, C)
 
+        z1 = X.dot(W1) + b1
+        a1 = np.maximum(0, z1)
+        z2 = a1.dot(W2) + b2  #(N,C)
+        scores = z2
+
+        # If the targets are not given, we are done
         if y is None:
-            return scores
+            return scores 
     
+        # Compute the loss
         loss = None
-        # Softmax classifier loss
-        exp_scores = np.exp(scores - scores.max(axis=1, keepdims=True))
-        p = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-        loss = -np.sum(np.log(p[np.arange(N), y])) / N 
-            + 0.5*reg*(np.sum(np.square(W2)) + np.sum(np.square(W1)))
+        z2_exp = np.exp(z2 - np.max(z2, axis=1, keepdims=True))
+          # For each score element in each row, subtract the correct score
+          # axis=1 is across rows, keepdims=True keeps column vec output
+        a2 = z2_exp / np.sum(z2_exp, axis=1, keepdims=True)
+        loss = (-1/N) * np.sum(np.log(a2[np.arange(N),y])) \
+            + (reg/2) * (np.sum(np.square(W2)) + np.sum(np.square(W1)))
+
+        # Backward pass, compute gradients
+        grads = {}
+        dloss_n = 1/N 
+        a2_id = np.zeros_like(a2)
+        a2_id[range(N),y] = 1
+          # a2_id is an (N,C) indicator matrix mask to get correct labels
+        dz2 = dloss_n * (a2 - a2_id)
+        dW2 = a1.T.dot(dz2) + reg*W2  # 2*reg*W2?
+        db2 = np.sum(dz2, axis=0)
+
+        da1 = dz2.dot(W2.T)  #(N,H)
+        da1_id = np.zeros_like(da1) + (da1 > 0)
+          # da1_id is an (N,H) indicator matrix mask to get ReLU output
+        dz1 = da1 * da1_id 
+        dW1 = X.T.dot(dz1) + reg*W1  # 2*reg*W1?
+        db1 = np.sum(dz1, axis=0)
+
+        grads['W1'] = dW1
+        grads['b1'] = db1
+        grads['W2'] = dW2
+        grads['b2'] = db2
+
+        return loss, grads
+
+    def train(self, X, y, X_val, y_val,
+              learning_rate=1e-3, learning_rate_decay=0.95,
+              reg=1e-5, num_iters=100,
+              batch_size=200, verbose=False):
+        """
+        Train this neural network using stochastic gradient descent (SGD)
+        learning_rate, scalar giving learning rate for optimisation
+        learning_rate_decay, scalar giving factor used to decay the learning rate
+            after each epoch
+        reg, scalar giving regularisation strength
+        num_iters, number of steps to take when optimising
+        batch_size, number of training examples to use per step
+        verbose, boolean that prints progress during optimisation if true
+        """
+
+        num_train = X.shape[0]
+        iterations_per_epoch = max(num_train / batch_size, 1)
+
+        # Use SGD to optimise the parameters in self.model
+        loss_history = []
+        train_acc_history = []
+        val_acc_history = []
+
+        for it in range(num_iters):
+            # Create a random minibatch of training data and labels
+            X_batch = None
+            y_batch = None
+            index = np.random.choice(num_train, batch_size, replace=True)
+            X_batch = X[index]
+            y_batch = y[index]
+
+            #Compute loss and gradients using the current minibatch
+            loss, grads = self.loss(X_batch, y=y_batch, reg=reg)
+            loss_history.append(loss)
+
+            for key in grads.keys():
+                self.params[key] -= learning_rate * grads[key]
+
+            if verbose and it % 100 == 0:
+                print('iteration %d / %d: loss %f' % (it, num_iters, loss))
+
+            # Every epoch, check train and val accuracy and decay learning rate
+            if it % iterations_per_epoch == 0:
+                # Check accuracy
+                train_acc = (self.predict(X_batch) == y_batch).mean()
+                val_acc = (self.predict(X_val) == y_val).mean()
+                train_acc_history.append(train_acc)
+                val_acc_history.append(val_acc)
+                learning_rate *= learning_rate_decay
+    
+        return {
+            'loss_history': loss_history,
+            'train_acc_history': train_acc_history,
+            'val_acc_history': val_acc_history,
+        }
+
+    def predict(self, X):
+        y_pred = None
+        a1 = np.maximum(0, X.dot(self.params['W1']) + self.params['b1'])
+        z2 = a1.dot(self.params['W2']) + self.params['b2']
+        y_pred = np.argmax(z2, axis=1)
+        return y_pred
